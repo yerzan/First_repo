261a074ff9eca650e86d42c1ced65ec0e718343b
Misha Kleban <mishakleban32@gmail.com>
2024-03-21 22:41:34 +0200

    homework2.py

--- 1.py/homework2.py
+++ 1.py/homework2.py
@@ -2,15 +2,16 @@ import random
 
 def get_numbers_ticket(min_val, max_val, quantity):
     if not (1 <= min_val <= max_val <= 1000 and quantity > 0):
+        print("Параметри не відповідають вимогам.")
         return []
-    
+
     numbers_set = set()
 
     while len(numbers_set) < quantity:
-        numbers_set.add(random.randint(min_val,max_val))
+        numbers_set.add(random.randint(min_val, max_val))
 
     numbers_list = sorted(numbers_set)
     return numbers_list
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
-print('Ваші лотерейні білети', lottery_numbers)
+print("Ваші лотерейні числа:", lottery_numbers)
